42c8e317bd2450dc7f99e3054f00c23cdb9deee7
@krikun1983 <akhmadeevst@gmail.com>
2021-08-24 23:03:07 +0400

    feat: add about page style

--- react-router/src/containers/about-page/index.tsx
+++ react-router/src/containers/about-page/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const AboutPage = (): JSX.Element => {
   return (
-    <>
-      <div>Hello AboutPage component</div>
-    </>
+    <section className="about">
+      <h2 className="about__heading">The Rick and Morty API</h2>
+      <p className="about__text">
+        The Rick and Morty API is a REST(ish) and GraphQL API based on the television show Rick and Morty. You will have
+        access to about hundreds of characters, images, locations and episodes. The Rick and Morty API is filled with
+        canonical information as seen on the TV show.
+      </p>
+    </section>
   );
 };
 
