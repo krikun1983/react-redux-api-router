3b571d289492b3764c525f6236013260852a7224
@krikun1983 <akhmadeevst@gmail.com>
2021-08-24 22:50:52 +0400

    feat: add style not-found page

--- react-router/src/containers/not-found-page/index.tsx
+++ react-router/src/containers/not-found-page/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useLocation } from 'react-router';
+import img404 from '../../assets/images/404.png';
 
 const NotFoundPage = (): JSX.Element => {
+  const location = useLocation();
+
   return (
-    <>
-      <div>Hello NotFoundPage component</div>
-    </>
+    <div className="not-found">
+      <img className="not-found__img" src={img404} alt="404" />
+      <p className="not-found__text">
+        The page- <span>{`{ ${location.pathname} }`}</span> does not exist...
+      </p>
+    </div>
   );
 };
 
