6575e2475fcc7cb03249613b56ccd3dc7eb41c12
@krikun1983 <akhmadeevst@gmail.com>
2021-08-29 11:31:52 +0400

    feat: add compose-with-dev-tools

--- react-redux/src/store/index.ts
+++ react-redux/src/store/index.ts
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import think from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(think));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(think)));
 
 export default store;
